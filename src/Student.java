32c67e6e040007b276be1e49c4395c5329c22f81
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-20 13:07:13 -0500

    nj

--- src/Student.java
+++ src/Student.java
@@ -2,12 +2,12 @@ public class Student implements Comparable<Student> {
     private String name;
     private double gpa;
 
-    public Student(String name, double age) {
+    public Student(String name, double gpa) {
         this.name = name;
-        this.gpa = age;
+        this.gpa = gpa;
     }
 
     @Override
     public int compareTo(Student other) {
-        return Integer.compare(this.gpa, other.gpa);    }
+        return Double.compare(this.gpa, other.gpa);    }
 }
